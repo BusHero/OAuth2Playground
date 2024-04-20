27d3d11d02afc4f800da96f9e1d9ec7f83b3e34d
bus1hero <petru.cervac@gmail.com>
2024-04-20 13:13:40 +0300

    save current status

--- WebResource/Program.cs
+++ WebResource/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,12 +19,83 @@ app.MapPost("/resource", async (
     HttpContext context) =>
 {
     var token = await extractor.GetBearerToken(context);
-    return new { Message = token };
+    var bearerToken = GetParts(token);
+    return new
+    {
+        BearerToken = bearerToken,
+        Token = token,
+    };
 });
 
 app.Run();
+return;
+
+BearerToken GetParts(string? token)
+{
+    if (token is null)
+    {
+        return new BearerToken();
+    }
+    
+    var parts = token.Split(".");
+
+    if (parts.Length != 3)
+    {
+        return new BearerToken();
+    }
+
+    var header = parts[0].FromBase64();
+    var content = parts[1].FromBase64();
+
+    var bearerToken = new BearerToken
+    {
+        Header = JsonSerializer.Deserialize<Header>(header),
+        Content = JsonSerializer.Deserialize<Content>(content),
+    };
+
+    return bearerToken;
+}
 
 public partial class Program;
+
+public static class Base64Extensions
+{
+    public static string FromBase64(this string base64)
+    {
+        var bytes = Convert.FromBase64String(base64);
+        var @string = Encoding.UTF8.GetString(bytes);
+        return @string;
+    }
+}
+
+public class BearerToken
+{
+    public Header? Header { get; set; }
+
+    public Content? Content { get; set; }
+}
+
+public class Header
+{
+    [JsonPropertyName("typ")] public string? Type { get; set; }
+
+    [JsonPropertyName("alg")] public string? Algorithm { get; set; }
+}
+
+public class Content
+{
+    [JsonPropertyName("iss")] public string? Issuer { get; set; }
+
+    [JsonPropertyName("sub")] public string? Subject { get; set; }
+
+    [JsonPropertyName("aud")] public string? Audience { get; set; }
+
+    [JsonPropertyName("iat")] public int? IssuedAt { get; set; }
+
+    [JsonPropertyName("exp")] public int? ExpirationTime { get; set; }
+
+    [JsonPropertyName("jti")] public string? JwtId { get; set; }
+}
 
 internal class TokenExtractor(ILogger<TokenExtractor> logger)
 {
--- WebResourceTests/UnitTest1.cs
+++ WebResourceTests/UnitTest1.cs
@@ -1,28 +1,31 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using AutoFixture.Xunit2;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace WebResourceTests;
 
-public class UnitTest1(WebApplicationFactory<Program> factory) 
+public class UnitTest1(WebApplicationFactory<Program> factory)
     : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _client = factory.CreateDefaultClient();
 
-    [Theory, AutoData]
-    public async Task RightAuthorizationSchemeReturnsToken(string token)
+    [Fact]
+    public async Task RightAuthorizationSchemeReturnsToken()
     {
+        var token = GetToken();
         _client.DefaultRequestHeaders.Authorization
             = new AuthenticationHeaderValue("Bearer", token);
-        
+
         var result = await _client
-            .PostAsync("/resource", null) ;
-        
+            .PostAsync("/resource", null);
+
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .Be(token);
     }
@@ -31,101 +34,158 @@ public class UnitTest1(WebApplicationFactory<Program> factory)
     public async Task NoAuthHeaderReturnsNull()
     {
         var result = await _client
-            .PostAsync("/resource", null) ;
-        
+            .PostAsync("/resource", null);
+
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .BeNull();
     }
 
     [Theory, AutoData]
-    public async Task InvalidAuthReturnsNull(string scheme, string token)
+    public async Task InvalidAuthReturnsNull(string scheme)
     {
+        var token = GetToken();
         _client.DefaultRequestHeaders.Authorization
             = new AuthenticationHeaderValue(scheme, null);
-        
+
         var result = await _client
-            .PostAsync("/resource", null) ;
-        
+            .PostAsync("/resource", null);
+
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .BeNull();
     }
 
     [Theory, AutoData]
-    public async Task WrongAuthSchemeReturnsNull(string scheme, string token)
+    public async Task WrongAuthSchemeReturnsNull(string scheme)
     {
+        var token = GetToken();
         _client.DefaultRequestHeaders.Authorization
             = new AuthenticationHeaderValue(scheme, token);
-        
+
         var result = await _client
-            .PostAsync("/resource", null) ;
-        
+            .PostAsync("/resource", null);
+
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .BeNull();
     }
-    
-    [Theory, AutoData]
-    public async Task GetTokenFromBody(string token)
+
+    [Fact]
+    public async Task GetTokenFromBody()
     {
+        var token = GetToken();
         var result = await _client
             .PostAsync("/resource", new FormUrlEncodedContent([
                 new KeyValuePair<string, string>("access_token", token)
-            ])) ;
+            ]));
 
         var body = await result.Content.ReadAsStringAsync();
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .Be(token);
     }
-    
+
     [Theory, AutoData]
-    public async Task NoTokenInBodyReturnsNull(string otherName, string token)
+    public async Task NoTokenInBodyReturnsNull(string otherName)
     {
+        var token = GetToken();
         var result = await _client
             .PostAsync("/resource", new FormUrlEncodedContent([
                 new KeyValuePair<string, string>(otherName, token)
-            ])) ;
+            ]));
 
         var body = await result.Content.ReadAsStringAsync();
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .BeNull();
     }
-    
-    [Theory, AutoData]
-    public async Task GetTokenFromQueryParameters(string token)
+
+    [Fact]
+    public async Task GetTokenFromQueryParameters()
     {
+        var token = GetToken();
+
         var result = await _client
-            .PostAsync($"/resource?access_token={token}", null) ;
+            .PostAsync($"/resource?access_token={token}", null);
 
         var body = await result.Content.ReadAsStringAsync();
         var content = await result.Content.ReadFromJsonAsync<Content>();
-        
+
         content!
-            .Message
+            .Token
             .Should()
             .Be(token);
+    }
+
+    [Fact]
+    public async Task JwtToken()
+    {
+        var token = GetToken(
+            issuer: "http://localhost:9001");
+        _client.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+
+        var result = await _client
+            .PostAsync("/resource", null);
+
+        var content = await result.Content.ReadFromJsonAsync<BearerToken>();
+
+        content!
+            .Content!
+            .Issuer
+            .Should()
+            .Be("http://localhost:9001");
+    }
+
+    private static string GetToken(
+        string? issuer = default)
+    {
+        var header =
+            Convert.ToBase64String(
+                Encoding.UTF8.GetBytes(
+                    JsonSerializer.Serialize(new
+                    {
+                        typ = "JWT",
+                        alg = "none",
+                    })));
+        var payload =
+            Convert.ToBase64String(
+                Encoding.UTF8.GetBytes(
+                    JsonSerializer.Serialize(new
+                    {
+                        iss = issuer ?? "http://localhost:9001",
+                        sub = "alice",
+                        aud = "http://localhost:9002/",
+                        iat = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                        exp = DateTimeOffset.Now.AddMinutes(5).ToUnixTimeSeconds(),
+                        jti = Guid.NewGuid().ToString("N"),
+                    })));
+
+        var token = $"{header}.{payload}.";
+
+        return token;
     }
 }
 
 internal class Content
 {
-    public string Message { get; set; }
+    public BearerToken BearerToken { get; set; } = null!;
+    
+    public string? Token { get; set; }
 }
