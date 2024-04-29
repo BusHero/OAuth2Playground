0e0ffffbf028ce12a76844187359e3a68d731e2d
bus1hero <petru.cervac@gmail.com>
2024-04-30 00:11:03 +0300

    chore: format authorization service

--- AuthorizationServer/Program.cs
+++ AuthorizationServer/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddSingleton<IClientRepository, InMemoryClientRepository>();
-builder.Services.AddTransient<IValidator<Request?>, RequestValidator>();
+builder.Services.AddTransient<IValidator<Request>, RequestValidator>();
 
 builder.Services
     .AddSingleton<IRequestsRepository, InMemoryRequestsRepository>();
